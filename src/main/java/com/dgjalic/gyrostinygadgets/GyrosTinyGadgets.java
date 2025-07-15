91efac7c69246867aa89cdd2fb0167b5bc5252fb
GyroZepelix <gamingskullgamer@gmail.com>
2025-07-15 21:25:28 +0200

    feat: added flint and iron cooldown, usable flint, particles, chance not to light

--- src/main/java/com/dgjalic/gyrostinygadgets/GyrosTinyGadgets.java
+++ src/main/java/com/dgjalic/gyrostinygadgets/GyrosTinyGadgets.java
@@ -1,11 +1,14 @@
 package com.dgjalic.gyrostinygadgets;
 
+import com.dgjalic.gyrostinygadgets.config.ModCommonConfigs;
 import com.dgjalic.gyrostinygadgets.item.ModItemProperties;
 import com.dgjalic.gyrostinygadgets.item.ItemRegistry;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.ForgeEventFactory;
 import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
@@ -24,6 +27,9 @@ public class GyrosTinyGadgets
         MinecraftForge.EVENT_BUS.register(this);
 
         modEventBus.addListener(this::clientSetup);
+
+        context.registerConfig(ModConfig.Type.CLIENT, ModCommonConfigs.SPEC, "gyrostinygadgets-client.toml");
+        context.registerConfig(ModConfig.Type.COMMON, ModCommonConfigs.SPEC, "gyrostinygadgets-common.toml");
     }
 
     private void clientSetup(final FMLClientSetupEvent event)
