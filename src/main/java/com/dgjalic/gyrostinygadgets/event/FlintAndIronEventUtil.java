4174c461554eeff763e891fc0a2851993f3c9111
GyroZepelix <gamingskullgamer@gmail.com>
2025-07-15 21:25:28 +0200

    feat: added flint and iron cooldown, usable flint, particles, chance not to light

--- src/main/java/com/dgjalic/gyrostinygadgets/event/FlintAndIronEventUtil.java
+++ src/main/java/com/dgjalic/gyrostinygadgets/event/FlintAndIronEventUtil.java
@@ -1,15 +1,14 @@
 package com.dgjalic.gyrostinygadgets.event;
 
-import net.minecraft.advancements.CriteriaTriggers;
+import com.dgjalic.gyrostinygadgets.config.ModCommonConfigs;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
-import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.core.particles.ParticleTypes;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.InteractionHand;
-import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.BaseFireBlock;
 import net.minecraft.world.level.block.CampfireBlock;
@@ -18,49 +17,78 @@ import net.minecraft.world.level.block.CandleCakeBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraft.world.level.gameevent.GameEvent;
+import net.minecraft.world.phys.Vec3;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 
 public class FlintAndIronEventUtil {
-    public static boolean useOn(PlayerInteractEvent.RightClickBlock event) {
+    public static void useOn(PlayerInteractEvent.RightClickBlock event) {
         Player player = event.getEntity();
         Level level = event.getLevel();
-        BlockPos blockpos = event.getPos();
-        BlockState blockstate = level.getBlockState(blockpos);
+        BlockPos eventBlockpos = event.getPos();
+        BlockState blockstate = level.getBlockState(eventBlockpos);
+        double randomChance = Math.random();
 
         Direction useFace = event.getFace();
-        if (useFace == null) {
-            return false;
+        boolean isMainOnCooldown = player.getCooldowns().isOnCooldown(player.getMainHandItem().getItem());
+        if (useFace == null || isMainOnCooldown) {
+            return;
         }
 
-        if (!CampfireBlock.canLight(blockstate) && !CandleBlock.canLight(blockstate) && !CandleCakeBlock.canLight(blockstate)) {
-            BlockPos blockpos1 = blockpos.relative(event.getFace());
-            if (BaseFireBlock.canBePlacedAt(level, blockpos1, getHorizontalDirection(player))) {
-                level.playSound(player, blockpos1, SoundEvents.FLINTANDSTEEL_USE, SoundSource.BLOCKS, 1.0F, level.getRandom().nextFloat() * 0.4F + 0.8F);
-                BlockState blockstate1 = BaseFireBlock.getState(level, blockpos1);
-                level.setBlock(blockpos1, blockstate1, 11);
-                level.gameEvent(player, GameEvent.BLOCK_PLACE, blockpos);
-                swingBothHands(player);
+        playSparkAnimation(event, level, player, eventBlockpos);
 
-                return true;
-            } else {
-                return false;
-            }
-        } else {
-            level.playSound(player, blockpos, SoundEvents.FLINTANDSTEEL_USE, SoundSource.BLOCKS, 1.0F, level.getRandom().nextFloat() * 0.4F + 0.8F);
-            level.setBlock(blockpos, blockstate.setValue(BlockStateProperties.LIT, Boolean.valueOf(true)), 11);
-            level.gameEvent(player, GameEvent.BLOCK_CHANGE, blockpos);
-            swingBothHands(player);
-
-            return true;
+        if (level.isClientSide()) {
+            return;
         }
+        if (randomChance < ModCommonConfigs.FLINT_AND_IRON_BREAK_CHANCE.get()) {
+            destroyFlint(player, level, eventBlockpos);
+        }
+        if (randomChance < 1-ModCommonConfigs.FLINT_AND_IRON_LIGHT_CHANCE.get()) {
+            return;
+        }
+
+        boolean isLightableBlock = CampfireBlock.canLight(blockstate) || CandleBlock.canLight(blockstate) || CandleCakeBlock.canLight(blockstate);
+        if (isLightableBlock) {
+            lightLightableBlock(level, eventBlockpos, blockstate, player);
+            return;
+        }
+
+        BlockPos relativeBlockpos = eventBlockpos.relative(event.getFace());
+        if (BaseFireBlock.canBePlacedAt(level, relativeBlockpos, getHorizontalDirection(player))) {
+            lightBlock(level, player, relativeBlockpos, eventBlockpos);
+        }
+    }
+
+    private static void playSparkAnimation(PlayerInteractEvent.RightClickBlock event, Level level, Player player, BlockPos eventBlockpos) {
+        level.playSound(player, eventBlockpos, SoundEvents.FLINTANDSTEEL_USE, SoundSource.BLOCKS, 1.0F, level.getRandom().nextFloat() * 0.4F + 0.8F);
+        player.swing(InteractionHand.MAIN_HAND);
+        if (event.getLevel() instanceof ServerLevel serverLevel) {
+            Vec3 facingVector = player.getViewVector(1);
+            double x = player.getX() + facingVector.x * .8;
+            double y = player.getY() + 1.1;
+            double z = player.getZ() + facingVector.z * .8;
+            serverLevel.sendParticles(ParticleTypes.SMALL_FLAME, x, y, z, 1, 0.1, 0.1, 0.1, 0);
+        }
+        player.getCooldowns().addCooldown(player.getMainHandItem().getItem(), ModCommonConfigs.FLINT_AND_IRON_USE_COOLDOWN.get());
+        player.getCooldowns().addCooldown(player.getOffhandItem().getItem(), ModCommonConfigs.FLINT_AND_IRON_USE_COOLDOWN.get());
+    }
+
+    private static void lightLightableBlock(Level level, BlockPos eventBlockpos, BlockState blockstate, Player player) {
+        level.setBlock(eventBlockpos, blockstate.setValue(BlockStateProperties.LIT, Boolean.valueOf(true)), 11);
+        level.gameEvent(player, GameEvent.BLOCK_CHANGE, eventBlockpos);
+    }
+
+    private static void lightBlock(Level level, Player player, BlockPos relativeBlockpos, BlockPos eventBlockpos) {
+        BlockState blockstate1 = BaseFireBlock.getState(level, relativeBlockpos);
+        level.setBlock(relativeBlockpos, blockstate1, 11);
+        level.gameEvent(player, GameEvent.BLOCK_PLACE, eventBlockpos);
+    }
+    
+    private static void destroyFlint(Player player, Level level, BlockPos eventBlockpos) {
+        player.getItemInHand(InteractionHand.OFF_HAND).shrink(1);
+        level.playSound(null, eventBlockpos, SoundEvents.STONE_BREAK, SoundSource.BLOCKS, 1.0F, level.getRandom().nextFloat() * 0.4F + 0.8F);
     }
 
     private static Direction getHorizontalDirection(Player player) {
         return player == null ? Direction.NORTH : player.getDirection();
-    }
-
-    private static void swingBothHands(Player player) {
-        player.swing(InteractionHand.OFF_HAND);
-        player.swing(InteractionHand.MAIN_HAND );
     }
 }
